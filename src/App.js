60372f8972f6a7dd366cbbb0080422d86848eb1b
shehan82 <shehansandeepa82@gmail.com>
2021-09-07 08:32:16 +0530

    remove animation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Clock from "./components/Clock";
 import Animation from "./components/Animation";
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
     <div className="App">
-      <Animation />
+      <MainSection />
       {/* <Clock /> */}
-      <div style={{ height: 800 }}></div>
+      <div style={{ height: 800, width: 10, backgroundColor: "red" }}></div>
     </div>
   );
 }
---
+++ src/components/MainSection.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import * as THREE from "three";
+import "../styles/animation.css";
+import logo from "../img/logo.png";
+import Clock from "../components/Clock";
+import NavBar from "../components/NavBar";
+import Typewriter from "typewriter-effect";
+
+function MainSection() {
+  const [display, setdisplay] = useState(false);
+
+  const displayNavBar = () => {
+    // console.log(window.scrollY);
+    if (window.scrollY > 0) {
+      setdisplay(true);
+    } else if (window.scrollY <= 0) {
+      setdisplay(false);
+    }
+  };
+
+  window.addEventListener("scroll", displayNavBar);
+  return (
+    <div className="hgx">
+      <div className="container">
+        <NavBar display={display} />
+        <div className="logo-cnt"></div>
+
+        <div className="content">
+          {/* <img className="logo" src={logo} alt="" /> */}
+          <h1 className="main-text">IntelliHack '21</h1>
+          {/* <p>Artificial, But Inteligent</p> */}
+          <div className="type-writer">
+            <Typewriter
+              options={{ autoStart: true, loop: true }}
+              onInit={(typewriter) => {
+                typewriter
+
+                  .typeString(
+                    "First ever machine learning hackerthon sri Lanka"
+                  )
+                  .callFunction(() => {
+                    console.log("String typed out!");
+                  })
+                  .pauseFor(2500)
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .typeString("Artificial, But Inteligent")
+                  .pauseFor(2500)
+                  .start();
+              }}
+            />
+          </div>
+
+          <div className="clock-div">
+            <Clock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
