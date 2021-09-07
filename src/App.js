f5bed07d961797b540757f0f698bc6cfed4d53c1
PunsisiPerera <punsisi.subhani@gmail.com>
2021-09-08 02:21:26 +0530

    mock time-line

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 import Clock from "./components/Clock";
 import Animation from "./components/Animation";
 import MainSection from "./components/MainSection";
+import Timeline from "./components/Timeline";
 
 function App() {
   return (
     <div className="App">
       <MainSection />
       {/* <Clock /> */}
-      <div style={{ height: 800, width: 10, backgroundColor: "red" }}></div>
+      <div>
+      <Timeline/>
+      </div>
+
     </div>
   );
 }
