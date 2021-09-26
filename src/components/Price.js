a788222b55f079d4f45d06eed0072a2fb88c1a03
YohanNayanajith <yohannayanajith40@gmail.com>
2021-09-26 21:20:44 +0530

    Correct price and title

--- src/components/Price.js
+++ src/components/Price.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import First_Place from '../img/first_place.png';
 import Second_Place from '../img/second_place.png';
 import Third_Place from '../img/third_place.png';
-import Titles from '../img/titles1.png';
+import Titles from '../img/title2.png';
 import "../styles/price.css";
 
 const Price = () => {
@@ -48,19 +48,32 @@ const Price = () => {
             <div className="titles-row-master">
                 {/* <h1>Title row</h1> */}
                 <div className="intellihack-master-image">
+   
                     <img className="title-card-img-top1" src={Titles} alt="intellihack-master"/>
+                    <div class="title-card-img-top1-text">
+                        <div>INTELLIHACK'21<br />MASTER</div>
+                    </div>
                 </div>
-                <div className="intellihack-master">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim asperiores nemo nam? Facere obcaecati, nesciunt ex, atque tempore, quasi omnis reprehenderit totam consequuntur non ullam natus. Dolorum a exercitationem eius quidem accusamus? Aspernatur in inventore quis natus voluptates minus sint. Repudiandae asperiores, eaque dolorum reiciendis doloremque dolore debitis ipsum atque iure impedit at aut!
+                <div className="intellihack-master-text">
+                    <div className="intellihack-master">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim asperiores nemo nam? Facere obcaecati, nesciunt ex, atque tempore, quasi omnis reprehenderit totam consequuntur non ullam natus. Dolorum a exercitationem eius quidem accusamus? Aspernatur in inventore quis natus voluptates minus sint. Repudiandae asperiores, eaque dolorum reiciendis doloremque dolore debitis ipsum atque iure impedit at aut!
+                    </div>
                 </div>
             </div>
 
             <div className="titles-row-most-popular">
-                <div className="most-popular-team">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim asperiores nemo nam? Facere obcaecati, nesciunt ex, atque tempore, quasi omnis reprehenderit totam consequuntur non ullam natus. Dolorum a exercitationem eius quidem accusamus? Aspernatur in inventore quis natus voluptates minus sint. Repudiandae asperiores, eaque dolorum reiciendis doloremque dolore debitis ipsum atque iure impedit at aut!
-                </div>
+                
                 <div className="most-popular-team-image">
-                    <img className="title-card-img-top2" src={Titles} alt="intellihack-master"/>
+
+                    <img className="title-card-img-top1" src={Titles} alt="intellihack-master"/>
+                    <div class="title-card-img-top2-text">
+                        <div>MOST POPULAR<br />TEAM</div>
+                    </div>
+                </div>
+                <div className="intellihack-most-popular-text">
+                    <div className="most-popular-team">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim asperiores nemo nam? Facere obcaecati, nesciunt ex, atque tempore, quasi omnis reprehenderit totam consequuntur non ullam natus. Dolorum a exercitationem eius quidem accusamus? Aspernatur in inventore quis natus voluptates minus sint. Repudiandae asperiores, eaque dolorum reiciendis doloremque dolore debitis ipsum atque iure impedit at aut!
+                    </div>
                 </div>
             </div>
 
