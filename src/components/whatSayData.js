3218499d8cb8b5dd26b5dfb50e10d7b507bfdaa0
PunsisiPerera <punsisi.subhani@gmail.com>
2021-09-27 02:25:34 +0530

    WHAT THEY SAY fixes

---
+++ src/components/whatSayData.js
@@ -0,0 +1,22 @@
+export const multiData = [
+    {
+        Image:"https://source.unsplash.com/featured/?macbook",
+        Name: "John Doe",
+        Disc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        Image:"https://source.unsplash.com/featured/?iphone",
+        Name: "John Doe",
+        Disc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        Image:"https://source.unsplash.com/featured/?washingmachine",
+        Name: "John Doe",
+        Disc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        Image:"https://source.unsplash.com/featured/?vase",
+        Name: "John Doe",
+        Disc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+];
