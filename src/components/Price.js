f04a3dffed4181d24f816b7356335585d94176af
shehan82 <shehansandeepa82@gmail.com>
2021-09-27 23:49:31 +0530

    changes price

--- src/components/Price.js
+++ src/components/Price.js
@@ -4,6 +4,9 @@ import Second_Place from "../img/second_place.png";
 import Third_Place from "../img/third_place.png";
 import Titles from "../img/title2.png";
 import "../styles/price.css";
+import Flip from "react-reveal/Flip";
+import Roll from "react-reveal/Roll";
+import Slide from "react-reveal/Slide";
 
 const Price = () => {
   return (
@@ -12,62 +15,72 @@ const Price = () => {
         <div>Prices</div>
       </div>
       <div className="winners-row">
-        <div className="third-place">
-          <h1>Third Place</h1>
-          <div className="third_card">
-            <img
-              className="third-card-img-top"
-              id="image1"
-              src={Third_Place}
-              alt="third_place"
-            />
+        <Flip right>
+          <div className="third-place">
+            <h1>Third Place</h1>
+            <div className="third_card">
+              <img
+                className="third-card-img-top"
+                id="image1"
+                src={Third_Place}
+                alt="third_place"
+              />
+            </div>
+            <div className="third_card-detail">Rs. 25000</div>
           </div>
-          <div className="third_card-detail">Rs. 25000</div>
-        </div>
+        </Flip>
 
-        <div className="first-place">
-          <h1>First Place</h1>
-          <div className="third_card">
-            <img
-              className="third-card-img-top"
-              src={First_Place}
-              alt="first_place"
-            />
+        <Flip left>
+          <div className="first-place">
+            <h1>First Place</h1>
+            <div className="third_card">
+              <img
+                className="third-card-img-top"
+                src={First_Place}
+                alt="first_place"
+              />
+            </div>
+            <div className="second_card-detail">Rs. 75000</div>
           </div>
-          <div className="second_card-detail">Rs. 75000</div>
-        </div>
+        </Flip>
+
         {/* #041149 */}
-        <div className="second-place">
-          <h1>Second Place</h1>
-          <div className="third_card">
-            <img
-              className="third-card-img-top"
-              src={Second_Place}
-              alt="second_place"
-            />
+
+        <Flip right>
+          <div className="second-place">
+            <h1>Second Place</h1>
+            <div className="third_card">
+              <img
+                className="third-card-img-top"
+                src={Second_Place}
+                alt="second_place"
+              />
+            </div>
+            <div className="first_card-detail">Rs. 50000</div>
           </div>
-          <div className="first_card-detail">Rs. 50000</div>
-        </div>
+        </Flip>
       </div>
-      <div className="header-name2">
-        <div>Titles</div>
-      </div>
+      <div className="header-name2">{/* <div>Titles</div> */}</div>
       <div className="titles-row-master">
         {/* <h1>Title row</h1> */}
-        <div className="intellihack-master-image">
-          <img
-            className="title-card-img-top1"
-            src={Titles}
-            alt="intellihack-master"
-          />
-          <div class="title-card-img-top1-text">
-            <div>
-              INTELLIHACK'21
-              <br />
-              MASTER
+
+        <Slide left>
+          <div className="intellihack-master-image">
+            <img
+              className="title-card-img-top1"
+              src={Titles}
+              alt="intellihack-master"
+            />
+            <div class="title-card-img-top1-text">
+              <div>
+                INTELLIHACK'21
+                <br />
+                MASTER
+              </div>
             </div>
           </div>
-        </div>
+        </Slide>
+
         <div className="intellihack-master-text">
           <div className="intellihack-master">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim
@@ -82,20 +95,22 @@ const Price = () => {
       </div>
 
       <div className="titles-row-most-popular">
-        <div className="most-popular-team-image">
-          <img
-            className="title-card-img-top1"
-            src={Titles}
-            alt="intellihack-master"
-          />
-          <div class="title-card-img-top2-text">
-            <div>
-              MOST POPULAR
-              <br />
-              TEAM
+        <Slide right>
+          <div className="most-popular-team-image">
+            <img
+              className="title-card-img-top1"
+              src={Titles}
+              alt="intellihack-master"
+            />
+            <div class="title-card-img-top2-text">
+              <div>
+                MOST POPULAR
+                <br />
+                TEAM
+              </div>
             </div>
           </div>
-        </div>
+        </Slide>
         <div className="intellihack-most-popular-text">
           <div className="most-popular-team">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id enim
